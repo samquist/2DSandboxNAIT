ef22a889a98705a881bdb2d96a241c9d8fdc3053
nfootz <n.footz@gmail.com>
2026-02-23 13:41:39 -0700

    removed offset of the pin's connection point

--- Assets/Scripts/Touch Screen/Touch Drag & Pinch.cs
+++ Assets/Scripts/Touch Screen/Touch Drag & Pinch.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 //[RequireComponent(typeof(Collider2D))]
-//[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class DragAndScale : MonoBehaviour
 {
     [Header("Throw Behaviour")]
