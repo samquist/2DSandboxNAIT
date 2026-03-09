99642185541168b5b4f48eff92716e103ab26eb9
nfootz <n.footz@gmail.com>
2026-03-09 14:10:39 -0600

    added pinching to interactibleObject

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -5,19 +5,19 @@ public abstract class InteractableObject : MonoBehaviour
     public virtual bool isDragging { get; protected set; }
 
     public virtual void OnGrabBegin()
-    {
-        return;
-    }
+    { return; }
 
     public virtual void OnGrabBegin(Vector2 screenPos)
-    {
-        return;
-    }
+    { return; }
 
     public abstract void OnGrabUpdate(Vector2 screenPos);
     public abstract void OnGrabEnd();
+
+    public virtual void OnPinchStart()
+    { return; }
+
+    public virtual void OnPinchUpdate(float currentDist)
+    { return; }
     public virtual void OnPinchEnd()
-    {
-        return;
-    }
+    {  return; }
 }
