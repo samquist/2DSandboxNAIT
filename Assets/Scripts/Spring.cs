3efdf49bd9b0356e874dfadfc8d4dd1ce176c1d9
nfootz <n.footz@gmail.com>
2026-03-14 17:26:51 -0600

    Spring scales properly...
    But doesn't connect or rotate or move sideways.

--- Assets/Scripts/Spring.cs
+++ Assets/Scripts/Spring.cs
@@ -14,17 +14,25 @@ public class Spring : MonoBehaviour
         bottomRB.AddForce(-forceVector.normalized * CalculateRestorativeForce(forceVector.magnitude) * (1 - friction) * Time.deltaTime / 2);
         topRB.AddForce(forceVector.normalized * CalculateRestorativeForce(forceVector.magnitude) * (1 - friction) * Time.deltaTime / 2);
 
-        springModel.localScale = new Vector3(springModel.localScale.x, forceVector.magnitude / topBlock.localScale.y, springModel.localScale.x);
+        springModel.localScale = new Vector3(springModel.localScale.x, forceVector.magnitude / topBlock.localScale.y / LRest, springModel.localScale.x);
         springModel.localPosition = new Vector3(springModel.localPosition.x, -0.55f * springModel.localScale.y, springModel.localPosition.z);
 
-        //topBlock.localPosition = new Vector3(0, topBlock.localPosition.y, topBlock.localPosition.z);
-        //topBlock.localEulerAngles = Vector3.zero;
-        //bottomBlock.localPosition = new Vector3(0, bottomBlock.localPosition.y, bottomBlock.localPosition.z);
-        //bottomBlock.localEulerAngles = Vector3.zero;
+        //RotateObjectsFaceEachOtherY(topBlock, bottomBlock);
     }
 
     public float CalculateRestorativeForce(float l)
     {
         return K * (l - LRest);
     }//end of CalculateRestorativeForce
+
+    private void RotateObjectsFaceEachOtherY(Transform t1, Transform t2)
+    {
+        t1.eulerAngles = new Vector3(0, 0, GetAngle(new Vector2(t1.position.x, t1.position.y), new Vector2(t2.position.x, t2.position.y)) + 90);
+        t2.eulerAngles = new Vector3(0, 0, GetAngle(new Vector2(t2.position.x, t2.position.y), new Vector2(t1.position.x, t1.position.y)) + 90);
+    }
+
+    private float GetAngle(Vector3 p1, Vector3 p2)
+    {
+        return Mathf.Atan2(p2.y - p1.y, p2.x - p1.x) * 180.0f / Mathf.PI;
+    }
 }
